14c5db7a7234a34d3bf334b5ddeb133847bb2e37
Michel115 <91220948+Michel115@users.noreply.github.com>
2021-11-09 19:08:20 +0200

    .

--- lib/page/Login.dart
+++ lib/page/Login.dart
@@ -4,7 +4,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_client/api/my_api.dart';
 import 'package:flutter_client/widgets/Button/myButton.dart';
+<<<<<<< Updated upstream
 import 'package:flutter_client/widgets/Other/errorAlertDialog.dart';
+=======
+import 'package:flutter_client/widgets/Code/codeDialog.dart';
+import 'package:flutter_client/widgets/Code/sixCode.dart';
+import 'package:flutter_client/widgets/Other/ErrorAlertDialog.dart';
+import 'package:flutter_client/widgets/TextInput/myErrorText.dart';
+>>>>>>> Stashed changes
 import 'package:flutter_client/widgets/TextInput/myTextInput.dart';
 import 'package:flutter_client/globals/globals.dart' as globals;
 
@@ -17,7 +24,12 @@ Color colPass = globals.blue;               //password
 Color colPass_1 = globals.blue_1;
 Color colPass_2 = globals.blue_2;
 
-
+String errTxtEmail = '';    //email error
+Color colErrTxtEmail = globals.transparent;
+String errTxtPass = '';     //password error
+Color colErrTxtPass = globals.transparent;
+String errTxt = '';         //else error
+Color colErrTxt = globals.transparent;
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -65,6 +77,8 @@ class _loginState extends State<login> {
                       }),
                 ),
 
+                myErrorText(errorText: errTxtEmail, color: colErrTxtEmail),
+
                 Padding(
                   padding: const EdgeInsets.only(left: 20.0, top: 8.0, right: 20.0, bottom: 8.0),
                   child: myTextInput(
@@ -84,6 +98,8 @@ class _loginState extends State<login> {
                   ),
                 ),
 
+                myErrorText(errorText: errTxtPass, color: colErrTxtPass),
+
                 Padding(
                   padding: const EdgeInsets.all(28.0),
                   child: InkWell(
@@ -100,6 +116,8 @@ class _loginState extends State<login> {
                     },
                   ),
                 ),
+
+                myErrorText(errorText: errTxt, color: colErrTxt),
 
                 Padding(
                   padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
@@ -132,66 +150,60 @@ class _loginState extends State<login> {
 
 
   _LoginCtrl(){
-    bool if1 = false;
-    bool if2 = false;
+    bool isEmpty = false;
 
-    if(globals.emailLogin != null){
-      if(globals.emailLogin!.isNotEmpty)
-        if1 = true;
-    }
+     errTxtEmail = '';
+     errTxtPass = '';
 
-    if(globals.passwordLogin != null){
-      if(globals.passwordLogin!.isNotEmpty)
-        if2 = true;
-    }
 
-    if(if1){
+    if(globals.emailLogin != null && globals.emailLogin != ''){
       setState(() {
         colEmail = Colors.blue.shade50;
         colEmail_1 = Colors.blue.shade900;
         colEmail_2 = Colors.blue.shade900.withOpacity(0.5);
       });
     }else{
+      isEmpty = true;
       setState(() {
         colEmail = Colors.red.shade50;
         colEmail_1 = Colors.red.shade900;
         colEmail_2 = Colors.red.shade900.withOpacity(0.5);
+        errTxtEmail = globals.error7;
+        colErrTxtEmail = globals.red_1;
       });
     }
 
-
-    if(if2){
+    if(globals.passwordLogin != null && globals.passwordLogin != ''){
       setState(() {
         colPass = Colors.blue.shade50;
         colPass_1 = Colors.blue.shade900;
         colPass_2 = Colors.blue.shade900.withOpacity(0.5);
       });
     }else{
+      isEmpty = true;
       setState(() {
         colPass = Colors.red.shade50;
         colPass_1 = Colors.red.shade900;
         colPass_2 = Colors.red.shade900.withOpacity(0.5);
+        errTxtPass = globals.error7;
+        colErrTxtPass = globals.red_1;
       });
     }
 
-
-    if(if1 && if2){
+    if(isEmpty == false){
       _verifc();
     }else{
-      showDialog(
-          context: context,
-          builder: (BuildContext context) => ErrorAlertDialog(
-              message: globals.error7));
+      //do nothing
     }
-
 
   }
 
 
 
   _verifc() async{
+    errTxt = '';
 
-    // try {
+    try {
       // if(globals.emailLogin != null && globals.passwordLogin != null){
       // print(globals.emailLogin);
       // print(globals.passwordLogin);
@@ -208,41 +220,28 @@ class _loginState extends State<login> {
       List<dynamic> body = json.decode(res.body);
 
       if (body[0] == "success") {
+<<<<<<< Updated upstream
         Navigator.pushNamed(context, '/Contrat');
+=======
+        // Navigator.pushNamed(context, '/home');
+        showDialog(
+            context: context,
+            builder: (BuildContext context) => codeDialog()).then((exit) {
+          setState(() {
+            _nullTextCode();
+          });
+        });
+>>>>>>> Stashed changes
       }else if (body[0] == "errorToken") {
-        showDialog<String>(
-          context: context,
-          builder: (BuildContext context) =>
-              AlertDialog(
-                title: const Text('Error'),
-                content: const Text(
-                    globals.errorToken),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () =>
-                        Navigator.pop(context, 'OK'),
-                    child: const Text('OK'),
-                  ),
-                ],
-              ),
-        );
+        setState(() {
+          errTxt = globals.errorToken;
+          colErrTxt = globals.red_1;
+        });
       }else if (body[0] == "errorVersion") {
-        showDialog<String>(
-          context: context,
-          builder: (BuildContext context) =>
-              AlertDialog(
-                title: const Text('Error'),
-                content: const Text("Your version: " + globals.version + "\n" +
-                    globals.errorVersion),
-                actions: <Widget>[
-                  TextButton(
-                    onPressed: () =>
-                        Navigator.pop(context, 'OK'),
-                    child: const Text('OK'),
-                  ),
-                ],
-              ),
-        );
+        setState(() {
+          errTxt = "Your version: " + globals.version + "\n" + globals.errorVersion;
+          colErrTxt = globals.red_1;
+        });
       }else if (body[0] == "error8") {
         colEmail = Colors.red.shade50;
         colEmail_1 = Colors.red.shade900;
@@ -250,24 +249,31 @@ class _loginState extends State<login> {
         colPass = Colors.red.shade50;
         colPass_1 = Colors.red.shade900;
         colPass_2 = Colors.red.shade900.withOpacity(0.5);
-        showDialog(
-            context: context,
-            builder: (BuildContext context) =>
-                ErrorAlertDialog(
-                    message: globals.error8));
+        setState(() {
+          errTxt = globals.error8;
+          colErrTxt = globals.red_1;
+        });
       } else{
-        showDialog(
-            context: context,
-            builder: (BuildContext context) => ErrorAlertDialog(
-                message: globals.errorElse));
+        setState(() {
+          errTxt = globals.errorElse;
+          colErrTxt = globals.red_1;
+        });
       }
-    //
-    // }catch(e){
-    //   print(e);
-    //   showDialog(
-    //       context: context,
-    //       builder: (BuildContext context) => ErrorAlertDialog(
-    //           message: globals.errorException));
-    // }
+
+    }catch(e){
+      print(e);
+      setState(() {
+        errTxt = globals.errorException;
+        colErrTxt = globals.red_1;
+      });
+    }
+  }
+  _nullTextCode(){
+    firstNb = null;
+    secondNb = null;
+    thirdNb = null;
+    fourthNb = null;
+    fifthNb = null;
+    sixNb = null;
   }
 }
--- lib/widgets/Code/myCode.dart
+++ lib/widgets/Code/myCode.dart
@@ -1,74 +1,50 @@
-
-import 'dart:convert';
-
-import 'package:flutter_client/api/my_api.dart';
-import 'package:flutter_client/globals/globals.dart' as globals;
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_client/widgets/Button/myButton.dart';
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 class myCode extends StatelessWidget {
-
+  var textString;
   var onChange;
   var keybType;
-  var color, color_1, color_2;
+  var colBlue, colBlue_1, colBlue_2;
   var textInputAction;
+  var labelText;
 
   myCode(
-  {this.keybType,
-  this.textInputAction,
-  required this.color,
-  required this.color_1,
-  required this.color_2,
-  required this.onChange});
+      {required this.textString,
+        required this.labelText,
+        this.keybType,
+        this.colBlue,
+        this.colBlue_1,
+        this.colBlue_2,
+        this.onChange});
 
-    @override
-    Widget build(BuildContext context) {
-      return TextFormField(
-        onChanged: onChange,
-        textAlign: TextAlign.left,
-        cursorColor: Colors.black,
-        decoration: InputDecoration(
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: color),
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      maxLength: 1,
+      inputFormatters: [
+        FilteringTextInputFormatter.digitsOnly
+      ],
+      onChanged: onChange,
+      textAlign: TextAlign.left,
+      cursorColor: Colors.black,
+      decoration: InputDecoration(
+        hintText: this.textString,
+        hintStyle: TextStyle(fontSize: 14.0, color: colBlue_1),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colBlue),
             borderRadius: BorderRadius.circular(10.0)),
-          filled: true,
-          fillColor: color,
-          border: InputBorder.none,
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: color_1),
-          borderRadius: BorderRadius.circular(10)),
-        ),
-        textInputAction: textInputAction,
-        keyboardType: this.keybType,
-      );
-    }
+        filled: true,
+        fillColor: colBlue,
+        labelText: labelText,
+        alignLabelWithHint: true,
+        labelStyle: TextStyle(fontSize: 14.0,color: colBlue_1),
+        border: InputBorder.none,
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colBlue_1),
+            borderRadius: BorderRadius.circular(10)),
+      ),
+      keyboardType: this.keybType,
+    );
   }
-
-  // _checkCode() async{
-  //   try {
-  //     // if(globals.emailLogin != null && globals.passwordLogin != null){
-  //     // print(globals.emailLogin);
-  //     // print(globals.passwordLogin);
-  //     var data = {
-  //       'code': globals.sixCode,
-  //     };
-  //
-  //     var res = await CallApi().postData(
-  //         data, 'Registration/Model/(Model)registMail.inc.php');
-  //     print(res);
-  //     print(res.body);
-  //     List<dynamic> body = json.decode(res.body);
-  //
-  //     if()
-  //
-  //
-  //   }catch(e){
-  //
-  //   }
-  // }
+}
