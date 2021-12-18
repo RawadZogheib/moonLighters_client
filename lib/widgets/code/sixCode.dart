5f19053d9abf5f96c9db9e99f8cadb77bbead5c1
Michel115 <91220948+Michel115@users.noreply.github.com>
2021-12-18 10:39:43 +0200

    .

--- lib/widgets/code/sixCode.dart
+++ lib/widgets/code/sixCode.dart
@@ -23,6 +23,12 @@ class sixCode extends StatefulWidget {
 
 class _sixCodeState extends State<sixCode> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    errCode = '';
+  }
   Widget build(BuildContext context) {
     return Container(
         child: Column(
@@ -48,6 +54,34 @@ class _sixCodeState extends State<sixCode> {
                     myErrorText(errorText: errCode, color: colErrCode),
 
                     Padding(
+                      padding: EdgeInsets.only(top: 15),
+                      child: Row(
+                        children: [
+                          Row(
+                            children: [
+                              InkWell(
+                                child: Text("Resend Code",style: TextStyle(
+                                    color: Colors.blue
+                                ),),
+                                onTap: (){
+                                  //_nullLogin();
+                                  //Navigator.pushNamed(context, '/Registration');
+                                  _resendCode();
+                                },
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                 left: MediaQuery.of(context).size.width * 0.1),
+                                child: Text("0:00"),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+
+
+                    Padding(
                       padding: const EdgeInsets.all(18.0),
                       child: Container(
                           child: InkWell(
@@ -63,109 +97,6 @@ class _sixCodeState extends State<sixCode> {
               )]));
   }
 
-  // _sendCode() async {
-  //   try{
-  //     String? vCode;
-  //     // vCode = globals.code1! + globals.code2! + globals.code3! +
-  //     //     globals.code4! + globals.code5! + globals.code6!;
-  //     var data = {
-  //       'version': globals.version,
-  //       'email'  : globals.email,
-  //       'vCode'  : vCode
-  //     };
-  //     var res = await CallApi().postData(data, '(Control)getVCode.php');
-  //     List<dynamic> body = json.decode(res.body);
-  //     print(body[0]);
-  //     if(body[0] == "success"){
-  //       Navigator.pushNamedAndRemoveUntil(context, '/intro_page2', (route) => false);
-  //     } else if (body[0] == "errorVersion") {
-  //       showDialog<String>(
-  //         context: context,
-  //         builder: (BuildContext context) =>
-  //             AlertDialog(
-  //               title: const Text('Error'),
-  //               content: const Text("Your version: " + globals.version + "\n"+
-  //                   globals.errorVersion),
-  //               actions: <Widget>[
-  //                 TextButton(
-  //                   onPressed: () =>
-  //                       Navigator.pop(context, 'OK'),
-  //                   child: const Text('OK'),
-  //                 ),
-  //               ],
-  //             ),
-  //       );
-  //
-  //     }else if (body[0] == "errorToken") {
-  //       showDialog<String>(
-  //         context: context,
-  //         builder: (BuildContext context) =>
-  //             AlertDialog(
-  //               title: const Text('Error'),
-  //               content: const Text(
-  //                   globals.errorToken),
-  //               actions: <Widget>[
-  //                 TextButton(
-  //                   onPressed: () =>
-  //                       Navigator.pop(context, 'OK'),
-  //                   child: const Text('OK'),
-  //                 ),
-  //               ],
-  //             ),
-  //       );
-  //     } else if (body[0] == "false") {
-  //       showDialog<String>(
-  //         context: context,
-  //         builder: (BuildContext context) => AlertDialog(
-  //           title: const Text('Error'),
-  //           content: const Text(
-  //               'Wrong Code !'),
-  //           actions: <Widget>[
-  //             TextButton(
-  //               onPressed: () =>
-  //                   Navigator.pop(context, 'OK'),
-  //               child: const Text('OK'),
-  //             ),
-  //           ],
-  //         ),
-  //       );
-  //     } else {
-  //       showDialog<String>(
-  //         context: context,
-  //         builder: (BuildContext context) => AlertDialog(
-  //           title: const Text('Error'),
-  //           content: const Text(
-  //               globals.errorElse),
-  //           actions: <Widget>[
-  //             TextButton(
-  //               onPressed: () =>
-  //                   Navigator.pop(context, 'OK'),
-  //               child: const Text('OK'),
-  //             ),
-  //           ],
-  //         ),
-  //       );
-  //     }
-  //
-  //
-  //   }catch(e){
-  //     showDialog<String>(
-  //       context: context,
-  //       builder: (BuildContext context) => AlertDialog(
-  //         title: const Text('Error'),
-  //         content: const Text(
-  //             globals.errorException),
-  //         actions: <Widget>[
-  //           TextButton(
-  //             onPressed: () =>
-  //                 Navigator.pop(context, 'OK'),
-  //             child: const Text('OK'),
-  //           ),
-  //         ],
-  //       ),
-  //     );
-  //
-  //   }}
 
   _checkCode() async{
     errCode = '';
@@ -203,6 +134,40 @@ class _sixCodeState extends State<sixCode> {
       errCode = globals.errorException;
       colErrCode = globals.red_1;
     }
+  }
+
+
+  _resendCode() async{
+    errCode = "";
+    colErrCode = globals.transparent;
+
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    globals.email = localStorage.getString('email');
+    var data = {
+      'version': globals.version,
+      'email': globals.email,
+    };
+
+    var res = await CallApi().postData(
+        data, 'Login/Control/(Control)resendMail.php');
+    print(res);
+    //print("pppppp");
+    List<dynamic> body = json.decode(res.body);
+    print(res.body);
+
+    if(body[0] == "error2_5"){
+      errCode = globals.error2_5;
+      colErrCode = globals.red_1;
+    }else if(body[0] == "codeException"){
+      errCode = globals.codeException;
+      colErrCode = globals.red_1;
+    }else{
+      setState(() {
+        errCode = globals.errorElse;
+        colErrCode = globals.red_1;
+      });
+    }
+
   }
 
 }
